ce2aa0c1b12c9e9cc26aecfb592863b90d76a12c
Dennis van Blanken <99040363@mydavinci.nl>
2017-02-15 12:02:44 +0100

    Game is done.

--- Js/script.js
+++ Js/script.js
@@ -41,24 +41,51 @@ function setcolor4(ele) {
 }
 
 function showAnswer() {
-
+	alert("Congratulations, You Win!");
+	for (var i = 0; i<=3; i++) {
+		var colour = theCode[i];
+		switch(colour) {
+			case "red":
+				document.getElementById('answer' + i).style.background = 'red';
+				break;
+			case "green":
+				document.getElementById('answer' + i).style.background= 'green';
+				break;
+			case "blue":
+				document.getElementById('answer' + i).style.background= 'blue';
+				break;
+			case "yellow":
+				document.getElementById('answer' + i).style.background= 'yellow';
+				break;
+			case "grey":
+				document.getElementById('answer' + i).style.background = 'grey';
+				break;
+			case "white":
+				document.getElementById('answer' + i).style.background = 'white';
+				break;
+		}
+	}
 }
 
 function CheckAnswer() {
+	guesses = [Guess1, Guess2, Guess3, Guess4];
 	if (guesses[0] == theCode[0] & guesses[1] == theCode[1] & guesses[2] == theCode[2] & guesses[3] == theCode[3]) {
 		showAnswer();
 	} else {
 		CheckPos1();
 		if (row <=9) {
-			document.getElementById("Row" + row + "Pos1").click = "";
-			document.getElementById("Row" + row + "Pos2").click = "";
-			document.getElementById("Row" + row + "Pos3").click = "";
-			document.getElementById("Row" + row + "Pos4").click = "";
-			row = row + 1;
-			document.getElementById("Row" + row + "Pos1").click = function() { setcolor1(this)};
-			document.getElementById("Row" + row + "Pos2").click = function() { setcolor2(this)};
-			document.getElementById("Row" + row + "Pos3").click = function() { setcolor3(this)};
-			document.getElementById("Row" + row + "Pos4").click = function() { setcolor4(this)};
+			console.log(row)
+			document.getElementById("Row" + row + "Pos1").onclick = "";
+			document.getElementById("Row" + row + "Pos2").onclick = "";
+			document.getElementById("Row" + row + "Pos3").onclick = "";
+			document.getElementById("Row" + row + "Pos4").onclick = "";
+			row++;
+			console.log(row)
+			document.getElementById("Row" + row + "Pos1").onclick = function() { setcolor1(this)};
+			document.getElementById("Row" + row + "Pos2").onclick = function() { setcolor2(this)};
+			document.getElementById("Row" + row + "Pos3").onclick = function() { setcolor3(this)};
+			document.getElementById("Row" + row + "Pos4").onclick = function() { setcolor4(this)};
+			console.log(document.getElementById("Row" + row + "Pos1"))
 		} else {
 			showAnswer(); // <===== not done
 		}
@@ -68,22 +95,24 @@ function CheckAnswer() {
 function CheckPos1() {
 	if (guesses[0] == theCode[0]) {
 		CheckPos2();
-		document.getElementById("Row" + row + "Status1").style.background = "red";
+		console.log("test")
+		document.getElementById("Row" + row + "Status2").style.background = "red";
 	} else {
 		if (guesses[0] == theCode[1] || guesses[0] == theCode[2] || guesses[0] == theCode[3]) {
-			document.getElementById("Row" + row + "Status1").style.background = "White";
+			document.getElementById("Row" + row + "Status2").style.background = "White";
 		}
 		CheckPos2();
+		console.log("test1")
 	}
 }
 
 function CheckPos2() {
 	if (guesses[1] == theCode[1]) {
 		CheckPos3();
-		document.getElementById("Row" + row + "Status2").style.background = "red";
+		document.getElementById("Row" + row + "Status1").style.background = "red";
 	} else {
 		if (guesses[1] == theCode[0] || guesses[1] == theCode[2] || guesses[1] == theCode[3]) {
-			document.getElementById("Row" + row + "Status2").style.background = "White";
+			document.getElementById("Row" + row + "Status1").style.background = "White";
 		}
 		CheckPos3();
 	}
@@ -92,10 +121,10 @@ function CheckPos2() {
 function CheckPos3() {
 	if (guesses[2] == theCode[2]) {
 		CheckPos4();
-		document.getElementById("Row" + row + "Status3").style.background = "red";
+		document.getElementById("Row" + row + "Status4").style.background = "red";
 	} else {
 		if (guesses[2] == theCode[0] || guesses[2] == theCode[1] || guesses[2] == theCode[3]) {
-			document.getElementById("Row" + row + "Status3").style.background = "White";
+			document.getElementById("Row" + row + "Status4").style.background = "White";
 		}
 		CheckPos4();
 	}
@@ -103,10 +132,10 @@ function CheckPos3() {
 
 function CheckPos4() {
 	if (guesses[3] == theCode[3]) {
-		document.getElementById("Row" + row + "Status4").style.background = "red";
+		document.getElementById("Row" + row + "Status3").style.background = "red";
 	} else {
 		if (guesses[3] == theCode[0] || guesses[3] == theCode[1] || guesses[3] == theCode[2]) {
-			document.getElementById("Row" + row + "Status4").style.background = "White";
+			document.getElementById("Row" + row + "Status3").style.background = "White";
 		}
 	}
 }

636ccdee650928c0864b45fa6ec90f18e62a6173
Dennis van Blanken <99040363@mydavinci.nl>
2017-02-15 10:54:21 +0100

    Code improved

--- Js/script.js
+++ Js/script.js
@@ -6,11 +6,9 @@ var Guess1 = "";
 var Guess2 = "";
 var Guess3 = "";
 var Guess4 = "";
+var guesses = [];
+var row = 1;
 var status = 0;
-var Status1 = document.getElementById("Status1");
-var Status2 = document.getElementById("Status2");
-var Status3 = document.getElementById("Status3");
-var Status4 = document.getElementById("Status4");
 var myArray = ['red', 'green', 'blue', 'yellow', 'grey', 'white'];
 var theCode = [myArray[Math.floor(Math.random() * myArray.length)], myArray[Math.floor(Math.random() * myArray.length)], myArray[Math.floor(Math.random() * myArray.length)], myArray[Math.floor(Math.random() * myArray.length)]];
 var Found = 0;
@@ -42,73 +40,73 @@ function setcolor4(ele) {
     Guess4 = color;
 }
 
-function CheckAnswer() {
-	// put the Guesses in an array
-	var Guesses = [Guess1, Guess2, Guess3, Guess4];
-	// check for how many of the guesses are in the code
-	console.log(found1, found2, found3, found4);
-	if (theCode.includes(Guesses[0])) { Found = Number(Found) + 1}
-	if (theCode.includes(Guesses[1])) { Found = Number(Found) + 1}
-	if (theCode.includes(Guesses[2])) { Found = Number(Found) + 1}
-	if (theCode.includes(Guesses[3])) { Found = Number(Found) + 1}
-	console.log(Found)
-	// make the small circles white if there are any
-	if (Found === 1) {
-	Status2.style.background = "white";
-	}
-	if (Found === 2) {
-	Status2.style.background = "white";
-	Status1.style.background = "white";
-	}
-	if (Found === 3) {
-	Status2.style.background = "white";
-	Status1.style.background = "white";
-	Status4.style.background = "white";
-	}
-	if (Found === 4) {
-	Status2.style.background = "white";
-	Status1.style.background = "white";
-	Status4.style.background = "white";
-	Status3.style.background = "white";
-	}
-	// check for how many guesses are in the correct spot
-	if (Guesses[0] === theCode[0]) {
-		console.log("Guess1 is correct");
-		status = Number(status) + 1;
-	}
-	if (Guesses[1] === theCode[1]) {
-		console.log("Guess2 is correct");
-		status = Number(status) + 1;
-	}
-	if (Guesses[2] === theCode[2]) {
-		console.log("Guess3 is correct");
-		status = Number(status) + 1;
-	}
-	if (Guesses[3] === theCode[3]) {
-		console.log("Guess4 is correct");
-		status = Number(status) + 1;
-	}
-	if (Guesses[0,1,2,3] === theCode[0,1,2,3]) {
-		console.log("You Win!!");
-	}
-	// make the small circles red if there are any correct
-	if (status === "1") {
-		Status2.style.background = "red";
-	}
-	if (status === "2") {
-		Status1.style.background = "red";
-		Status2.style.background = "red";
-	}
-	if (status === "3") {
-		Status1.style.background = "red";
-		Status2.style.background = "red";
-		Status4.style.background = "red";
-	}
-	if (status === "4") {
-		Status1.style.background = "red";
-		Status2.style.background = "red";
-		Status3.style.background = "red";
-		Status4.style.background = "red";
-	}
+function showAnswer() {
 
+}
+
+function CheckAnswer() {
+	if (guesses[0] == theCode[0] & guesses[1] == theCode[1] & guesses[2] == theCode[2] & guesses[3] == theCode[3]) {
+		showAnswer();
+	} else {
+		CheckPos1();
+		if (row <=9) {
+			document.getElementById("Row" + row + "Pos1").click = "";
+			document.getElementById("Row" + row + "Pos2").click = "";
+			document.getElementById("Row" + row + "Pos3").click = "";
+			document.getElementById("Row" + row + "Pos4").click = "";
+			row = row + 1;
+			document.getElementById("Row" + row + "Pos1").click = function() { setcolor1(this)};
+			document.getElementById("Row" + row + "Pos2").click = function() { setcolor2(this)};
+			document.getElementById("Row" + row + "Pos3").click = function() { setcolor3(this)};
+			document.getElementById("Row" + row + "Pos4").click = function() { setcolor4(this)};
+		} else {
+			showAnswer(); // <===== not done
+		}
+	}
+}
+
+function CheckPos1() {
+	if (guesses[0] == theCode[0]) {
+		CheckPos2();
+		document.getElementById("Row" + row + "Status1").style.background = "red";
+	} else {
+		if (guesses[0] == theCode[1] || guesses[0] == theCode[2] || guesses[0] == theCode[3]) {
+			document.getElementById("Row" + row + "Status1").style.background = "White";
+		}
+		CheckPos2();
+	}
+}
+
+function CheckPos2() {
+	if (guesses[1] == theCode[1]) {
+		CheckPos3();
+		document.getElementById("Row" + row + "Status2").style.background = "red";
+	} else {
+		if (guesses[1] == theCode[0] || guesses[1] == theCode[2] || guesses[1] == theCode[3]) {
+			document.getElementById("Row" + row + "Status2").style.background = "White";
+		}
+		CheckPos3();
+	}
+}
+
+function CheckPos3() {
+	if (guesses[2] == theCode[2]) {
+		CheckPos4();
+		document.getElementById("Row" + row + "Status3").style.background = "red";
+	} else {
+		if (guesses[2] == theCode[0] || guesses[2] == theCode[1] || guesses[2] == theCode[3]) {
+			document.getElementById("Row" + row + "Status3").style.background = "White";
+		}
+		CheckPos4();
+	}
+}
+
+function CheckPos4() {
+	if (guesses[3] == theCode[3]) {
+		document.getElementById("Row" + row + "Status4").style.background = "red";
+	} else {
+		if (guesses[3] == theCode[0] || guesses[3] == theCode[1] || guesses[3] == theCode[2]) {
+			document.getElementById("Row" + row + "Status4").style.background = "White";
+		}
+	}
 }
